8ce48ec9f5ff4fc8d3f984f9056714007cc7cf08
Murat Alkan <muratalkanw@hotmail.com>
2022-05-11 07:20:31 +0300

    Apply api limiter

--- src/main.js
+++ src/main.js
@@ -15,13 +15,13 @@ const corsOptions = {
   origin: `*`
 };
 
-/*const apiLimiter = RateLimit({
+const apiLimiter = RateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
 	max: 100, // Limit each IP to 100 requests
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests!",
-})*/
+})
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({
@@ -29,7 +29,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cors(corsOptions));
-//app.use(apiLimiter); // apply rate limiter to all requests
+app.use(apiLimiter); // apply rate limiter to all requests
 app.use((request, response, next) => {
   next();
 });
